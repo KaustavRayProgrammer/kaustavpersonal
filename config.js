56bd07a26ab97f0b3482a8b99f35df4da188de66
KaustavRayProgrammer <kaustavray1987@gmail.com>
2017-02-28 18:02:21 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/test',
+    'facebook': {
+        clientID: '939176106196141',
+        clientSecret: 'ba163d3417dbe2878060a92990bff878',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
